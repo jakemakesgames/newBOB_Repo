5c2035aa4b5b3041521b24aa908ef89a3a67f37a
jakemakesgames <jakebrown.designer@gmail.com>
2018-07-29 20:08:06 +1000

    - basic player jump code is added and functional

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,12 +10,14 @@ public class Player : MonoBehaviour {
 	public float gravity = -20f;
 	public Vector3 velocity;
 	public float moveSpeed = 6;
+	public float jumpVelocity = 8;
 
 	void Start(){
 		controller = GetComponent<Controller2D> ();
-	}
+	} 	
 
 	void Update(){
+		
 		// This IF check stops the player from accumulating gravity, therefore stopping them from falling at a super fast speed
 		if (controller.collisions.above || controller.collisions.below) {
 			velocity.y = 0;
@@ -26,9 +28,19 @@ public class Player : MonoBehaviour {
 
 		velocity.x = input.x * moveSpeed;
 
+		// If the Spacebar is pressed call the Jump funtion
+		if (Input.GetKeyDown (KeyCode.Space) && controller.collisions.below) {
+			Jump ();
+		}
+
 		// Apply the gravity to the Velocity Vector's Y and multiply by Time.deltaTime
 		velocity.y += gravity * Time.deltaTime;
 		// Call the Move function on the controller and move the player using that method
 		controller.Move (velocity * Time.deltaTime);
+	}
+
+	void Jump(){
+		// Set the player's Velocity.Y equal to the Jump Velocity
+		velocity.y = jumpVelocity;
 	}
 }
