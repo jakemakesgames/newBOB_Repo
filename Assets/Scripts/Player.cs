357fe710cfb2c98da196a06e719fc461830ad5d9
jakemakesgames <jakebrown.designer@gmail.com>
2018-07-29 20:56:02 +1000

    - used an equation to solve the gravity and jump velocity using jumpHeight and timeToJumpApex variables for a smoother jump. - smoothed out the player movement along the X axis.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,13 +7,32 @@ public class Player : MonoBehaviour {
 
 	private Controller2D controller;
 
-	public float gravity = -20f;
-	public Vector3 velocity;
 	public float moveSpeed = 6;
-	public float jumpVelocity = 8;
+
+	// How high do we want the player to jump?
+	public float jumpHeight = 4;
+	// How long do we want the player to take to reach the Apex of the jump curve
+	public float timeToJumpApex = .4f;
+
+	[SerializeField] private float gravity;
+	[SerializeField] private float jumpVelocity;
+
+	public Vector3 velocity;
+	private float velocityXSmoothing;
+
+	private float acceleartionTimeAirbourne = .2f;
+	private float accelerationTimeGrounded = .1f;
+
 
 	void Start(){
 		controller = GetComponent<Controller2D> ();
+
+		// Calculating the gravity value
+		gravity = -(2 * jumpHeight) / Mathf.Pow (timeToJumpApex, 2);
+		// Calculating the Jump Velocity value
+		jumpVelocity = Mathf.Abs (gravity) * timeToJumpApex;
+
+		Debug.Log ("Gravity: " + gravity + " | Jump Velocity: " + jumpVelocity);
 	} 	
 
 	void Update(){
@@ -26,12 +45,14 @@ public class Player : MonoBehaviour {
 		// Store the Input in a Vector2 variable
 		Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
-		velocity.x = input.x * moveSpeed;
 
 		// If the Spacebar is pressed call the Jump funtion
 		if (Input.GetKeyDown (KeyCode.Space) && controller.collisions.below) {
 			Jump ();
 		}
+
+		float targetVelocityX = velocity.x = input.x * moveSpeed;
+		velocity.x = Mathf.SmoothDamp (velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below) ? accelerationTimeGrounded : acceleartionTimeAirbourne);
 
 		// Apply the gravity to the Velocity Vector's Y and multiply by Time.deltaTime
 		velocity.y += gravity * Time.deltaTime;
